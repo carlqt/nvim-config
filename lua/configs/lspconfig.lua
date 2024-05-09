76bdfe62dd0344e0bc91a336a6a7b8c4f9b38de9
Carl Tablante <carlwilliam.tablante@gmail.com>
2024-05-09 21:05:02 +1000

    feat: Added ruby_lsp

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "gopls" }
+local servers = { "html", "cssls", "gopls", "ruby_lsp" }
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
@@ -21,5 +21,3 @@ lspconfig.tsserver.setup {
   on_init = on_init,
   capabilities = capabilities,
 }
-
-lspconfig.ruby_lsp.setup{}
