83c8f5d00880567863aa287d8288c80e231331f8
Carl Tablante <carlwilliam.tablante@gmail.com>
2024-06-12 08:20:00 +1000

    fix: Updated keymaps for next/prev buffer

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -13,8 +13,8 @@ local options = { noremap = true, silent = true }
 vim.keymap.set("i", "jj", "<Esc>", options)
 
 -- Navigate buffer
-vim.keymap.set("n", "<C-u>", ":bp<cr>", { noremap = true, silent = true })
-vim.keymap.set("n", "<C-i>", ":bn<cr>", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-u>", ":bprevious<cr>", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-i>", ":bnext<cr>", { noremap = true, silent = true })
 
 -- Close buffer without losing split window
 vim.keymap.set("n", "<C-d>", ":bp|bd #<cr>", { noremap = true, silent = true })
@@ -25,7 +25,7 @@ vim.cmd.abbreviate("bonly", "%bd|e #<cr>")
 
 -- Shortcut keys for telescope plugin
 -- Helps finding files and text searches
-local builtin = require("telescope.builtin")
+local builtin = require "telescope.builtin"
 vim.keymap.set("n", "<C-p>", builtin.find_files, {})
 vim.keymap.set("n", "<C-f><C-f>", builtin.live_grep, {})
 
