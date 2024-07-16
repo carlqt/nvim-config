3d028715c45d346776445bf18eaa980df319dae0
Carl Tablante <ctablante@seek.com.au>
2024-07-16 22:01:05 +1000

    feat: Filter nvim tree

---
+++ lua/plugins/nvim-tree.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "nvim-tree/nvim-tree.lua",
+    opts = {
+      live_filter = {
+        prefix = "[FILTER]: ",
+        always_show_folders = false, -- Turn into false from true by default
+      },
+    },
+  },
+}
