2605e4262ddfe1b5525c8ed376c454a52db54ff2
Carl Tablante <carlwilliam.tablante@gmail.com>
2024-07-28 19:21:01 +1000

    feat: Refactoring configs and setup for lsp

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,40 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    config = true,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    opts = function()
+      -- Porting nvchad defaults for lsp config
+      local on_attach = require("nvchad.configs.lspconfig").on_attach
+      local on_init = require("nvchad.configs.lspconfig").on_init
+      local capabilities = require("nvchad.configs.lspconfig").capabilities
+
+      -- Adding vim-ufo fold capabilities
+      capabilities.textDocument.foldingRange = {
+        dynamicRegistration = false,
+        lineFoldingOnly = true,
+      }
+
+      local handlers = {
+        function(server_name)
+          require("lspconfig")[server_name].setup {
+            on_attach = on_attach,
+            on_init = on_init,
+            capabilities = capabilities,
+          }
+        end,
+      }
+
+      return {
+        handlers = handlers,
+      }
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+  },
+}
