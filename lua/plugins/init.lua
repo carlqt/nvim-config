8bb517d421284a76e9d238fbb6d64cb89bbf125a
Carl Tablante <carlwilliam.tablante@gmail.com>
2024-07-28 19:21:55 +1000

    feat: Refactoring configs and setup for lsp (#1)

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,37 +1,18 @@
 return {
   {
     "stevearc/conform.nvim",
-    event = 'BufWritePre', -- uncomment for format on save
+    event = "BufWritePre", -- uncomment for format on save
     config = function()
       require "configs.conform"
     end,
   },
 
   -- These are some examples, uncomment them if you want to see them work!
-  {
-  	"williamboman/mason.nvim",
-  	opts = {
-  		ensure_installed = {
-  			"lua-language-server", "stylua",
-  			"html-lsp", "css-lsp" , "prettier"
-  		},
-  	},
-  },
-
-  { "williamboman/mason-lspconfig.nvim" },
-
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require("nvchad.configs.lspconfig").defaults()
-      require "configs.lspconfig"
-    end,
-  },
   --
   --
   {
-      "nvim-treesitter/nvim-treesitter",
-      opts = {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
       ensure_installed = {
         "lua",
         "html",
