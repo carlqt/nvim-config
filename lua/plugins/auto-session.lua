f260bca14df34593354489a66d600c8fd2165628
Carl Tablante <ctablante@seek.com.au>
2024-08-22 09:13:37 +1000

    feat: Installed auto-session plugin

---
+++ lua/plugins/auto-session.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "rmagatti/auto-session",
+    lazy = false,
+    opts = {
+      auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+      -- log_level = 'debug',
+    },
+  },
+}
