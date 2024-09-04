6f86dedbea847fe832b0f5ff1ad5973e0a7c3b20
Carl Tablante <ctablante@seek.com.au>
2024-09-04 16:52:59 +1000

    feat: Update conform

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,3 +1,19 @@
+-- TODO: Move config to plugins. Similar to debugger structure
+
+-- Description: Conform format on command
+vim.api.nvim_create_user_command("Format", function(args)
+  local range = nil
+  if args.count ~= -1 then
+    local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+    range = {
+      start = { args.line1, 0 },
+      ["end"] = { args.line2, end_line:len() },
+    }
+  end
+  require("conform").format { async = true, lsp_format = "fallback", range = range }
+end, { range = true })
+
+-- Description: Conform options
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
@@ -7,11 +23,11 @@ local options = {
     go = { "goimports" },
   },
 
-  format_on_save = {
-    -- These options will be passed to conform.format()
-    timeout_ms = 500,
-    lsp_fallback = true,
-  },
+  -- format_on_save = {
+  --   -- These options will be passed to conform.format()
+  --   timeout_ms = 5000,
+  --   lsp_fallback = true,
+  -- },
 }
 
 require("conform").setup(options)
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,7 +1,8 @@
 return {
   {
     "stevearc/conform.nvim",
-    event = "BufWritePre", -- uncomment for format on save
+    -- event = "BufWritePre", -- uncomment for format on save
+    lazy = false,
     config = function()
       require "configs.conform"
     end,
