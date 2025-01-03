d14ae4f92d5b73c949fc75ecf5f18a62abfe2b57
Carl Tablante <ctablante@seek.com.au>
2025-01-03 14:30:10 +1100

    feat: Update NVChad to v3

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,11 +1,10 @@
 -- This file  needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/NvChad/blob/v2.5/lua/nvconfig.lua
 
----@type ChadrcConfig
 local M = {}
 
-M.ui = {
-  theme = "tokyodark", -- Main theme at home to avoid monitor flickering
+M.base46 = {
+  theme = "chadracula-evondev", -- Main theme at home to avoid monitor flickering
   -- theme = "chadracula-evondev", -- Usual theme
 
   hl_override = {
@@ -14,11 +13,6 @@ M.ui = {
       bg = "#3909bd",
     },
   },
-
-  -- hl_override = {
-  -- 	Comment = { italic = true },
-  -- 	["@comment"] = { italic = true },
-  -- },
 }
 
 return M
